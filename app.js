ca791c8599d5245089fa5d33be1bcba1a95a83e9
mpech <maxime.pech@freemo.blue>
2018-01-16 15:08:07 +0100

    #4472 support for push notif via websocket transport

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var app = exports = module.exports = express();
 var V = require('nodelibs').Validator;
 var S = require('nodelibs').Sanitizor();
 var slackNotifier = require('nodelibs').slackNotifier;
-
 var ParamChecker = require('nodelibs').ParamChecker;
 var APH = require('nodelibs').asyncPromiseHandler;
 var fs = require('fs');
+
 
 app.enable('trust proxy');
 app.disable('x-powered-by');
@@ -18,7 +18,7 @@ app.config = config;
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
+var WS = null;
 var db = require('diskdb');
 db = db.connect(config.dbpath, ['pushes']);
 setInterval(function cleanup(){
@@ -41,6 +41,7 @@ app.post('/generic-webhook-trigger/invoke', function(req, res, next){
         req.body = JSON.parse(req.body);
     }
     db.pushes.save(Object.assign({githubhook:{gid: gid, token: req.query.token, expiresAt: expiresAt}}, req.body));
+    WS && WS.send('push');
     return res.status(200).send('ok');
 });
 
@@ -71,8 +72,17 @@ app.use(function(req, res, next){
 });
 
 if(!module.parent){
-    app.listen(app.config.port, function(){
-        console.info('http on '+app.config.port);
+    var http = require('http');
+    const server = http.createServer(app);
+
+    var WebSocket = require('ws');
+    const wss = new WebSocket.Server({ server });
+    wss.on('connection', function(ws) {
+        WS = ws;
+    });
+
+    server.listen(config.port, function listening() {
+      console.log('Listening on %d', server.address().port);
     });
 }
 
